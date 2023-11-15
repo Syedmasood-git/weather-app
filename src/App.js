4f6b97c2422b13ac94797279f57e39d412838369
Syedmasood-git <masoodhussaini46@gmail.com>
2023-11-15 06:50:32 +0530

    Weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+const App = () => {
+    const [lat,setLat]=useState('')
+    const [long,setLong]=useState('')
+    const [hemsiphere,setHemisphere]=useState('');
+    const [month,setMonth]=useState( new Date().getMonth()+1)
+
+    function handleSubmit(){
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition((position)=>{
+                setLat(position.coords.latitude)
+                setLong(position.coords.longitude)
+
+                if(position.coords.latitude>0){
+                    setHemisphere("Northern Hemisphere")
+                }
+                else if(position.coords.latitude<0){
+                    setHemisphere("Southern Hemisphere")
+                }
+                else{
+                    setHemisphere("Equator")
+                }
+
+            })
+        }
+
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <h1>Weather App</h1>
+        <h2>Latitude:{lat}</h2>
+        <h2>Longitude:{long}</h2>
+        <h2>Hemisphere:{hemsiphere}</h2>
+        <button onClick={handleSubmit}>Get Location</button>
+
+        {
+           hemsiphere!=="" && (
+            (hemsiphere==="Northern Hemisphere" && (month>=4 && month<=10))|| 
+            (hemsiphere==="Southern Hemisphere" && (month<4 || month>10)) )
+           && (
+            <div>
+                <h1>It's Summer</h1>      
+            </div>
+           )
+      }
+
+      {
+           hemsiphere!=="" && ((hemsiphere==="Northern Hemisphere" && (month<4 || month>10))  ||(hemsiphere==="Southern Hemisphere" && (month>=4 && month<=10)))
+
+           && (
+            <div>
+                <h1>It's Winter</h1>     
+            </div>
+           )
+      }
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
